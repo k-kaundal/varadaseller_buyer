73a6cabbbbb3a922f9466fc52c932006d5901200
saleheen1 <smsaleheen3@gmail.com>
2023-03-09 10:42:01 +0600

    all string called in app strings from one file

--- lib/view/utils/others_helper.dart
+++ lib/view/utils/others_helper.dart
@@ -7,7 +7,7 @@ import 'package:qixer/view/utils/constant_colors.dart';
 
 //===========================>
 
-// String baseApi = 'https://beefinder.es/api/v1';
+// String baseApi = 'https://seekers.ng/api/v1';
 
 String baseApi = 'https://bytesed.com/laravel/qixer/api/v1';
 
